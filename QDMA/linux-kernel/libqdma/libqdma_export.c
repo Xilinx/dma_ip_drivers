0efc8795d0e6cc73edc12c156f8fe8a8def99116
Mark Harfouche <mark.harfouche@gmail.com>
2019-05-15 10:26:25 -0400

    partially valid qdma_queue_list is better than nothing on error.

--- QDMA/linux-kernel/libqdma/libqdma_export.c
+++ QDMA/linux-kernel/libqdma/libqdma_export.c
@@ -759,7 +759,6 @@ int qdma_queue_list(unsigned long dev_hndl, char *buf, int buflen)
 
 handle_truncation:
 	pr_warn("ERR! str truncated. req=%lu, avail=%u", cur - buf, buflen);
-	*buf = '\0';
 	return buf - cur;
 }
 
