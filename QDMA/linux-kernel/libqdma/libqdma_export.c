3508ba9d649a7cb3946cfe3e9d75bba005b94c92
Mark Harfouche <mark.harfouche@gmail.com>
2019-05-15 11:32:33 -0400

    undo returning a negative number rin ind_stm_cmd_op

--- QDMA/linux-kernel/libqdma/libqdma_export.c
+++ QDMA/linux-kernel/libqdma/libqdma_export.c
@@ -759,7 +759,7 @@ int qdma_queue_list(unsigned long dev_hndl, char *buf, int buflen)
 
 handle_truncation:
 	pr_warn("ERR! str truncated. req=%lu, avail=%u", cur - buf, buflen);
-	return buf - cur;
+	return cur - buf;
 }
 
 /*****************************************************************************/
