89141ecc8fa0977ce5e0a9a4a5c3f81020361aaf
Vladimir Komendantskiy <komendantsky@gmail.com>
2023-10-07 21:59:04 +0100

    iov_iter __iov field fix

--- XDMA/linux-kernel/xdma/cdev_sgdma.c
+++ XDMA/linux-kernel/xdma/cdev_sgdma.c
@@ -568,14 +568,22 @@ static ssize_t cdev_aio_read(struct kiocb *iocb, const struct iovec *io,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0)
 static ssize_t cdev_write_iter(struct kiocb *iocb, struct iov_iter *io)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+	return cdev_aio_write(iocb, io->__iov, io->nr_segs, io->iov_offset);
+#else
 	return cdev_aio_write(iocb, io->iov, io->nr_segs, io->iov_offset);
+#endif
 }
 
 static ssize_t cdev_read_iter(struct kiocb *iocb, struct iov_iter *io)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+	return cdev_aio_read(iocb, io->__iov, io->nr_segs, io->iov_offset);
+#else
 	return cdev_aio_read(iocb, io->iov, io->nr_segs, io->iov_offset);
-}
 #endif
+}
+#endif // LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0)
 
 static int ioctl_do_perf_start(struct xdma_engine *engine, unsigned long arg)
 {
