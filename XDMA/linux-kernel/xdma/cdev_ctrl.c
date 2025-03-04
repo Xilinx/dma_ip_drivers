e8ec40374c4c7831195c23109f586ecd03f6b11f
Apurv Padole <apurv.padole@amd.com>
2025-03-04 13:01:36 +0530

    Update for compilation issue caused by kernel update in Alma 9.4
    Signed-off-by: Apurv Padole <apurv.padole@amd.com>

--- XDMA/linux-kernel/xdma/cdev_ctrl.c
+++ XDMA/linux-kernel/xdma/cdev_ctrl.c
@@ -235,8 +235,14 @@ int bridge_mmap(struct file *file, struct vm_area_struct *vma)
 	 */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
         vm_flags_set(vma, VMEM_FLAGS);
+#elif defined(RHEL_RELEASE_CODE)
+	#if (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(9, 4))
+        vm_flags_set(vma, VMEM_FLAGS);
+	#else
+	vma->vm_flags |= VMEM_FLAGS;
+	#endif
 #else
-        vma->vm_flags |= VMEM_FLAGS;
+	vma->vm_flags |= VMEM_FLAGS;
 #endif
 	/* make MMIO accessible to user space */
 	rv = io_remap_pfn_range(vma, vma->vm_start, phys >> PAGE_SHIFT,
